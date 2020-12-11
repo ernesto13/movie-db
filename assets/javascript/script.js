5529f7bf4b5f84bb2cc243e772aeda5e729dbed8
Ernesto Samaniego <esamaniego53@gmail.com>
2020-12-11 22:56:17 +0000

    added style, and fetch

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,11 @@
-// https://api.themoviedb.org/3/movie/550?api_key=9033c1b2b1a8df6282b474400f1a9935
+$(document).ready(function(){
 
 const API_KEY = '9033c1b2b1a8df6282b474400f1a9935';
+
+fetch(`https://api.themoviedb.org/3/movie/550?api_key=API_KEY`)
+.then(response => response.json())
+.then(data => console.log(data));
+
+});
+
 
